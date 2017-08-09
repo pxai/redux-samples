445dcd6a7aade6d96fa4f8f44cbe8c4e4f705d56
Pello X. Altadill <pello@pello.info>
2017-08-09 14:45:16 +0200

    added new cases to reducer, must check anyway

--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -1,7 +1,25 @@
 function todoReducer (state, action) {
   switch (action.type) {
-    case 'DELETE_TODO' :
+    case 'ADD_TODO':
+      return {
+          ...state.todos,
+          ...action.todo
+      };
+    case 'DELETE_TODO':
       return state.todos.filter(elem => elem.id !== action.todo.id);
+    case 'UPDATE_TODO':
+      return state.todos.map( (elem) => {
+                if(elem.id !== action.todo.id) {
+                    // This isn't the item we care about - keep it as-is
+                    return item;
+                }
+                
+                // Otherwise, this is the one we want - return an updated value
+                return {
+                    ...elem,
+                    ...action.todo
+                };    
+            });
     default: 
         return state;
   }
