5f005bdbe1b5c00cc4e7f240e5623d20dac266c7
pxai <pello_altadill@cuatrovientos.org>
2017-08-11 05:44:26 +0200

    Ok

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,20 @@
 import  React  from 'react'
 
 class Home extends React.Component {
+
+    addTask (e) {
+         e.preventDefault();
+        console.log(e);
+    }
     render () {
         return (
             <div id="home">
                 Welcome home
-                
+        <form onSubmit={this.addTask}>
+          <label>Task</label><br />
+          <input type="text" ref="task"  /><br />
+          <button>Generate</button>
+        </form>
           <ul>
                 { 
                    this.props.todos.map((todo) =>
